5d21cd4abccccc2fea7aa9decdbd1d2f84a011ab
snaketrap <b.sarnecki11@gmail.com>
2022-09-02 16:22:18 +0200

    skonczone || dodanie przycisku oraz skryptu do znikania oraz pokazywania zdjecia

--- app/src/main/java/com/example/jakasnazwa_01092022/MainActivity.kt
+++ app/src/main/java/com/example/jakasnazwa_01092022/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.jakasnazwa_01092022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var btn_pu = findViewById<Button>(R.id.btn_pu)
+        var obraz = findViewById<ImageView>(R.id.obraz)
+        var liczba = 1
+        btn_pu.setOnClickListener {
+
+            if (obraz.isVisible) {
+                obraz.visibility = View.INVISIBLE
+            }
+            else{
+                obraz.visibility = View.VISIBLE
+            }
+
+
+        }
     }
 }
